da60393c62b5339b34a47fb50e98c791ccc5fe49
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-13 23:17:20 -0300

    docs: added document

--- rich/test_print.py
+++ rich/test_print.py
@@ -1,5 +1,8 @@
-from rich import print
+# Documentação para buscar info
+# https://rich.readthedocs.io/en/latest/introduction.html
+from rich import print 
 
 # Esse comando [red][/red] muda a cor da parte selecionada no terminal
 # Usar :: permite usar emojis
 print("Olá [green]Mundo[green] :earth_americas:")
+print("Olá, [bold white on red]Pequeno Garfanhoto[/] :vulcan_salute:")
