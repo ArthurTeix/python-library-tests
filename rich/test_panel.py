b2728dc546145d22cfc0e5de0bfc373291405029
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-13 23:37:31 -0300

    docs: finish panel

--- rich/test_panel.py
+++ rich/test_panel.py
@@ -1,11 +1,10 @@
 from rich import print
 from rich.panel import Panel # panel: módulo  |  Panel: classe
 
-caixa = Panel("Painel usando rich")
-print(caixa)
-
 # No meu panel posso adicionar outros parâmetros
 # :title: msg no topo do panel
 # :style: cor do panel
-title = Panel("[red]Aqui vai seguir meu texto[/]", title='Título no topo', style="cyan")
+# :width: largura (não exige aspas)
+# :height: altura (não exige aspas)
+title = Panel("[red]Aqui vai seguir meu like[/] :+1:", title='Título no topo', style="cyan", width=40, height=5)
 print(title)
