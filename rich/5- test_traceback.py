7ca1e5e4641e0403c8c22b1b9191f02bb8b55aa4
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-14 21:29:53 -0300

    docs: finish notes traceback

--- rich/5- test_traceback.py
+++ rich/5- test_traceback.py
@@ -1,2 +1,12 @@
-from rich.traceback import install
+from rich.traceback import install # exibe os erros mais bem organizados
 install()
+
+def div(x, y):
+    return x / y
+
+print(div(4, 2))
+
+idade = int(input("Digite sua idade: ")) # uma variável int
+
+# vou inputar string e o erro será mostrado de uma ótima forma visual
+print(idade) 
