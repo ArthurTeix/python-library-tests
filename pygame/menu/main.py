7a1ec1649f71f0b68779729c09996e7a3daf73e9
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-05 17:17:39 -0300

    imagens e botoes

--- pygame/menu/main.py
+++ pygame/menu/main.py
@@ -16,11 +16,19 @@ TEXTO_COR = (255, 255, 255)
 resume_img = pygame.image.load('./pygame/menu/img/button_resume.png').convert_alpha()
 options_img = pygame.image.load('./pygame/menu/img/button_options.png').convert_alpha()
 quit_img = pygame.image.load('./pygame/menu/img/button_quit.png').convert_alpha()
+video_img = pygame.image.load('./pygame/menu/img/button_video.png').convert_alpha()
+audio_img = pygame.image.load('./pygame/menu/img/button_audio.png').convert_alpha()
+keys_img = pygame.image.load('./pygame/menu/img/button_keys.png').convert_alpha()
+back_img = pygame.image.load('./pygame/menu/img/button_back.png').convert_alpha()
 
 # instancias dos botoes
 resume_botao = Botao(304, 125, resume_img, 1)
 options_botao = Botao(297, 250, options_img, 1)
 quit_botao = Botao(336, 375, quit_img, 1)
+video_botao = Botao(226, 75, video_img, 1)
+audio_botao = Botao(225, 200, audio_img, 1)
+keys_botao = Botao(246, 325, keys_img, 1)
+back_botao = Botao(332, 450, back_img, 1)
 
 def desenha_texto(texto, fonte, texto_cor, x, y):
     img = fonte.render(texto, True, texto_cor)
