1411d25b8fb2f36e55d300e14c119a3892af4a26
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-05 17:09:26 -0300

    menu principal

--- pygame/menu/main.py
+++ pygame/menu/main.py
@@ -12,10 +12,19 @@ fonte = pygame.font.SysFont("arialblack", 40) # difinição de fonte e tamanho
 # definir cor
 TEXTO_COR = (255, 255, 255)
 
+# imagens
+resume_img = pygame.image.load('./pygame/menu/img/button_resume.png').convert_alpha()
+options_img = pygame.image.load('./pygame/menu/img/button_options.png').convert_alpha()
+quit_img = pygame.image.load('./pygame/menu/img/button_quit.png').convert_alpha()
+
+# instancias dos botoes
+resume_botao = Botao(304, 125, resume_img, 1)
+options_botao = Botao(297, 250, options_img, 1)
+quit_botao = Botao(336, 375, quit_img, 1)
+
 def desenha_texto(texto, fonte, texto_cor, x, y):
     img = fonte.render(texto, True, texto_cor)
     tela.blit(img, (x, y))
-
 
 run = True
 while run:
@@ -24,7 +33,12 @@ while run:
 
     # checando se jogo ta pausado
     if jogo_pausado:
-        pass # siplay menu
+        if resume_botao.desenhar_botao():
+            jogo_pausado = False
+        if options_botao.desenhar_botao():
+            pass
+        if quit_botao.desenhar_botao():
+            run = False
 
     else:
         desenha_texto("Aperte Espaço Para Pausar", fonte, TEXTO_COR, 90, 250)
