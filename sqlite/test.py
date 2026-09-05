c055eca3c9abae685b150be6c29f8deaf5785f05
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-04 21:29:17 -0300

    criação de tabelas

--- sqlite/test.py
+++ sqlite/test.py
@@ -2,3 +2,10 @@ import sqlite3
 
 conexao = sqlite3.connect("sqlite/banco.db")
 cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS contas_bancarias (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                titular TEXT NOT NULL,
+                saldo FLOAT NOT NULL,
+                cpf TEXT NOT NULL UNIQUE
+                )""")
