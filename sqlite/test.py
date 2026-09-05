1f4a190c7a11e41d787936e6003fe74cd808c6b6
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-05 17:57:50 -0300

    testando sql para banco de questoes

--- sqlite/test.py
+++ sqlite/test.py
@@ -3,16 +3,29 @@ import sqlite3
 conexao = sqlite3.connect("sqlite/banco.db")
 cursor = conexao.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS contas_bancarias (
+a = 'carro'
+
+cursor.execute(f"""CREATE TABLE IF NOT EXISTS perguntas_{a} (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                titular TEXT NOT NULL,
-                saldo FLOAT NOT NULL,
-                cpf TEXT NOT NULL UNIQUE
+                pergunta TEXT NOT NULL,
+                resposta INTEGER NOT NULL
                 )""")
 
-cursor.execute("""
-DELETE FROM contas_bancarias
-WHERE id = 2
+# cursor.execute("""
+# INSERT INTO perguntas_carro (pergunta, resposta)
+# VALUES ("O que faz o carro funcionar?", "motor")
+# """)
+
+
+# cursor.execute("SELECT * FROM perguntas_carro")
+cursor.execute("SELECT id, pergunta, resposta FROM perguntas_carro")
+res = cursor.fetchall()
+
+for r in res:
+    id, pergunta, resposta = r
+    print(f"""
+{id}º Pergunta: {pergunta}
+Resposta: {resposta}
 """)
 
 conexao.commit()
