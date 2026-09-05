474f35c35ac85a76445672ca0db0be5d787f2ea4
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-05 16:49:24 -0300

    modularização

--- pygame/menu/main.py
+++ pygame/menu/main.py
@@ -1,13 +1,10 @@
 import pygame
+from botao import tela, Botao
 
 pygame.init()
 
-# tela
-largura_tela = 800
-altura_tela = 600
-
-tela = pygame.display.set_mode((largura_tela, altura_tela))
-pygame.display.set_caption("Menu")
+# variaveis do jogo
+jogo_pausado = False
 
 # definir fonte
 fonte = pygame.font.SysFont("arialblack", 40) # difinição de fonte e tamanho
@@ -15,22 +12,27 @@ fonte = pygame.font.SysFont("arialblack", 40) # difinição de fonte e tamanho
 # definir cor
 TEXTO_COR = (255, 255, 255)
 
-
 def desenha_texto(texto, fonte, texto_cor, x, y):
     img = fonte.render(texto, True, texto_cor)
     tela.blit(img, (x, y))
+
 
 run = True
 while run:
 
     tela.fill((52, 78, 91)) # cor de fundo
 
-    desenha_texto("Aperte Espaço Para Pausar", fonte, TEXTO_COR, 90, 250)
+    # checando se jogo ta pausado
+    if jogo_pausado:
+        pass # siplay menu
+
+    else:
+        desenha_texto("Aperte Espaço Para Pausar", fonte, TEXTO_COR, 90, 250)
 
     for event in pygame.event.get():
         if (event.type == pygame.KEYDOWN):
             if (event.key == pygame.K_SPACE):
-                print('pause')
+                jogo_pausado = True
 
         if (event.type == pygame.QUIT):
             run = False
